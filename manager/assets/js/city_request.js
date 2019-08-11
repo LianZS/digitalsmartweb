01faea558e048bd00fa478e1fc2d492b000b1218
LianZS <2214057556@qq.com>
2019-08-11 14:49:15 +0800

    地区子页

--- manager/assets/js/city_request.js
+++ manager/assets/js/city_request.js
@@ -1,5 +1,6 @@
 function Get_province_city(){
 	var url="http://127.0.0.1:8000/attractions/admin/provinces"
+	var area_url ="http://127.0.0.1:8000/attractions/api/getRegionsByCity"
 	$.get(url,function(data,state){
 		data = data['data']
 		provinces = new Set();
@@ -12,7 +13,9 @@ function Get_province_city(){
 				if(data[i].province==item){
 					city = data[i].loaction
 					citypid= data[i].citypid
-					newLi="<li style='display: inline-block; padding: 5px 10px; white-space:nowrap;color:black' id= "+citypid+" >"+"<a href=''>"+city+"</a>"+"</li>"
+					href = "scencedetail.html?province="+item+"&"+"location="+city+"&citypid="+citypid
+					newLi="<li style='display: inline-block; padding: 5px 10px; white-space:nowrap;color:black' pid= "+citypid+" value="+item+" >"+
+					"<a href="+href+">"+city+"</a>"+"</li>"
 					
 					newLiElements+=newLi
 				}
@@ -22,5 +25,7 @@ function Get_province_city(){
 			newElement="	<li><ul style='display: inline;    white-space: nowrap;color: #45B0E3;' value="+item+">"+item+"    "+newLiElements+"</ul></li>" 
 			$("#province").append(newElement)
 		}
-	},'json')
+	
+	},'json');
+	
 }
