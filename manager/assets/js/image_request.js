841b533b2ac3a9ee9e3eeaafa31303f815c7ad66
LianZS <2214057556@qq.com>
2019-08-11 16:50:06 +0800

    图片链接

--- manager/assets/js/image_request.js
+++ manager/assets/js/image_request.js
@@ -8,14 +8,21 @@ function Cover_Image_reuqest(pid, name) {
 		
 		if(href == "") {
 			href="assets/img/none.png"
-			newElement = "	<div class='col-xs-12 col-sm-6 col-md-3' style='width: 25%;'><h3>"+name+"</h3><div class='card-panel' ><img src=" + href + " style='width: 100%;height:300px' /></div></div>"
+			newElement = "	<div class='col-xs-12 col-sm-6 col-md-3' id= "+pid+" style='width: 25%;'><h3>"+name+"</h3><div class='card-panel' ><img src=" + href + " style='width: 100%;height:300px' /></div></div>"
 			
 		} else {
 			href=image_url+href
-			newElement = "	<div class='col-xs-12 col-sm-6 col-md-3' style='width: 25%;'><h3>"+name+"</h3><div class='card-panel' ><img src=" + href + " style='width: 100%;height:300px' /></div></div>"
+			newElement = "	<div class='col-xs-12 col-sm-6 col-md-3' id= "+pid+"  style='width: 25%;'><h3>"+name+"</h3><div class='card-panel' ><img src=" + href + " style='width: 100%;height:300px' /></div></div>"
 			
 		}
 		$("#page-inner .row").append(newElement)
-
+		p ="#"+pid
+		$(p).click(function(){
+			href="scencedata.html?area="+name+"&pid="+pid
+			$(location).attr('href', href);
+		})
+		
+		
 	}, 'json')
+	
 }
