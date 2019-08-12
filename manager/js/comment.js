950c74018ad2341c051c1b20113ea34a4b61af76
LianZS <2214057556@qq.com>
2019-08-12 22:22:56 +0800

    编辑删除界面设计完成

--- manager/js/comment.js
+++ manager/js/comment.js
@@ -25,24 +25,55 @@ function Comment(pid) {
 function CommentRate(pid) {
 	//评论指数
 	this.url = "http://127.0.0.1:8000/attractions/api/getCommentRate?"
+	var tr_count=0;
 	$.get(this.url, {
 		"pid": pid
 	}, function(data) {
 		comments = data['comment']
-		len=comments.length
+		len = comments.length
+		var j = 0;
 		for(let i = 0; i < len; i++) {
 			item = comments[i]
 			adjectives = item['adjectives']
 			rate = item['rate']
-			tr="	<tr class='collection-item'><td class=' collection-item' id=comment-key"+i+">"+adjectives+"</td><td class=' collection-item' id=comment-grade"+i+">"+rate+"</td> <td class='collection-item'><a><li class='fa fa-edit fa-2x' id="+i+"></li></a></td></tr>"
+			tr = "<tr  class='collection-item' id='tr" + i + "'><td class='collection-item' id='comment-key" + i + "'>" + adjectives + "</td><td class=' collection-item' id='comment-grade" + i + "'>" + rate + "</td> <td style='text-align:right;'><a style='padding-right: 20%;'><li  class='fa fa-minus-circle fa-2x' id='del" + i + "'></li></a><a><li class='fa fa-edit fa-2x' id='edit" + i + "'></li></a></td></tr>"
 			$("#comment-rate").append(tr)
-			$('#'+i).click(function(){
-				 var keyword = document.getElementById("comment-key"+i).setAttribute("contenteditable", "true");
-				 var grade = document.getElementById("comment-grade"+i).setAttribute("contenteditable", "true");
-
-				
-				
+			j = i;
+			$('#edit' + i).click(function() {
+				var keyword = document.getElementById("comment-key" + i).setAttribute("contenteditable", "true");
+				document.getElementById("comment-key" + i).focus()
+				var grade = document.getElementById("comment-grade" + i).setAttribute("contenteditable", "true");
+			});
+			$('#del' + i).click(function() {
+				tr_count-=1
+				$("#tr" + i).remove()
 			})
+			tr_count+=1
 		}
+		$("#comment-rate").append("<tr class='collection-item'><td></td><td></td><td></td></tr>")
+		$("#add-tr").click(function() {
+			if(tr_count>=10){
+				alert("最大只能拥有十条数据")
+				return
+			}
+			j += 1;
+			tr_count+=1
+
+			let i=j;
+			tr="<tr class=\"collection-item\" id=\"tr"+j+"\"><td class=\"collection-item\" id=\"comment-key"+j+"\"></td><td class=\"collection-item\" id=\"comment-grade"+j+"\"></td> <td style=\"text-align:right;\"><a style=\"padding-right: 20%;\"><li class=\"fa fa-minus-circle fa-2x\" id=\"del"+j+"\"></li></a><a><li class=\"fa fa-edit fa-2x\" id=\"edit"+j+"\"></li></a></td></tr>"
+			$("#comment-rate").append(tr)
+			$('#edit' + i).click(function() {
+				var keyword = document.getElementById("comment-key" + i).setAttribute("contenteditable", "true");
+				document.getElementById("comment-key" + i).focus()
+				var grade = document.getElementById("comment-grade" + i).setAttribute("contenteditable", "true");
+			});
+			$("#del"+i).click(function() {
+				tr_count-=1
+				$("#tr" + i).remove()
+			});
+		});
+		$("#send").click(function(){
+			
+		})
 	}, "json")
 }
