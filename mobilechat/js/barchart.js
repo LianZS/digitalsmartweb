d13415f3458d8c1c24d72e2bbbe002a53f2ddb88
LianZS <2214057556@qq.com>
2019-08-15 19:46:04 +0800

    年龄分布图

---
+++ mobilechat/js/barchart.js
@@ -0,0 +1,111 @@
+function draw_barchart(idname,under_nineth,nin_twen,twe_thir,thir_four,four_fift,over_fift) {
+	let mychart = echarts.init(document.getElementById(idname));
+
+	var dataAxis = ['19岁以下', '19~25岁', '26~35岁', '36~45岁', '46~55岁', '55岁以上'];
+	var data = [under_nineth,nin_twen,twe_thir,thir_four,four_fift,over_fift];
+	var yMax = 1;
+	var dataShadow = [];
+
+	for(var i = 0; i < data.length; i++) {
+		dataShadow.push(yMax);
+	}
+
+	option = {
+		title: {
+			text: '年龄分布',
+					textStyle: {
+				color: "white",
+			},
+		},
+//	tooltip: {
+//			trigger: 'axis'
+//		},
+		xAxis: {
+			data: dataAxis,
+			axisLabel: {
+				inside: true,
+				textStyle: {
+					color: 'white'
+				}
+			},
+			axisTick: {
+				show: false
+			},
+			axisLine: {
+				show: false
+			},
+			z: 10
+		},
+		yAxis: {
+			axisLine: {
+				show: false
+			},
+			axisTick: {
+				show: false
+			},
+			axisLabel: {
+				textStyle: {
+					color: '#999'
+				}
+			}
+		},
+		dataZoom: [{
+			type: 'inside'
+		}],
+		series: [{ // For shadow
+				type: 'bar',
+				itemStyle: {
+					normal: {
+						color: 'rgba(0,0,0,0.05)'
+					}
+				},
+				barGap: '-100%',
+				barCategoryGap: '40%',
+				data: dataShadow,
+				animation: false
+			},
+			{
+				type: 'bar',
+				itemStyle: {
+					normal: {
+						color: new echarts.graphic.LinearGradient(
+							0, 0, 0, 1, [{
+									offset: 0,
+									color: '#83bff6'
+								},
+								{
+									offset: 0.5,
+									color: '#188df0'
+								},
+								{
+									offset: 1,
+									color: '#188df0'
+								}
+							]
+						)
+					},
+					emphasis: {
+						color: new echarts.graphic.LinearGradient(
+							0, 0, 0, 1, [{
+									offset: 0,
+									color: '#2378f7'
+								},
+								{
+									offset: 0.7,
+									color: '#2378f7'
+								},
+								{
+									offset: 1,
+									color: '#83bff6'
+								}
+							]
+						)
+					}
+				},
+				data: data
+			}
+		]
+	};
+
+	mychart.setOption(option);
+}
--- mobilechat/js/request.js
+++ mobilechat/js/request.js
@@ -5,17 +5,36 @@ function request() {
 		"appid": appid
 	}, function(data) {
 		sex_data = data['sex']
-		ddate = new Array()
-		bboy = new Array()
-		ggirl = new Array()
+		let ddate = new Array()
+		let bboy = new Array()
+		let ggirl = new Array()
+		
 		for(let i = 0; i < sex_data.length; i++) {
 			sex = sex_data[i]
+			
 			ddate[i] = sex['ddate']
 			bboy[i] = sex['boy']
 			ggirl[i] = sex['girl']
 			
 		}
+		let boy= parseInt(sex['boy']*100)
+		let girl = parseInt(sex['girl']*100)
+		$("#male-rate").text(boy+"%")
+		$("#female-rate").text(girl+"%")
 		draw_linechart("sex-chart",bboy,ggirl,ddate)
+		age_data=data['age']
+		
+		for(let i=0;i<age_data.length;i++){
+			ddate = age_data[i]['ddate']
+			under_nineth=age_data[i]['under_nineth']
+			nin_twen =age_data[i]['nin_twen']
+			twe_thir=age_data[i]['twe_thir']
+			thir_four=age_data[i]['thir_four']
+			four_fift=age_data[i]['four_fift']
+			over_fift=age_data[i]['over_fift']
+			
+		}
+		draw_barchart("age-chart",under_nineth,nin_twen,twe_thir,thir_four,four_fift,over_fift)
 
 	}, 'json')
 
