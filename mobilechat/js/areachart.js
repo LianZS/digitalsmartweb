eea86ec7002752ebfea63c515f29a2d2e3847ce0
LianZS <2214057556@qq.com>
2019-08-15 20:53:18 +0800

    文件移动

--- mobilechat/js/areachart.js
+++ mobilechat/js/areachart.js
@@ -1,69 +1,63 @@
+function draw_active_areachart(idname, ddate_array, active_array) {
+	var mychart = echarts.init(document.getElementById(idname));
+	option = {
+		title: {
+			textStyle: {
+				color: 'white'
+			}
+		},
+		tooltip: {
+			trigger: 'axis',
+			axisPointer: {
+				type: 'cross',
+				label: {
+					backgroundColor: '#6a7985'
+				}
+			}
+		},
+		xAxis: {
+			type: 'category',
+			name: "时间",
+			nameLocation: "middle",
+			nameGap: 30,
+			nameTextStyle: {
+				color: "white",
+				fontSize: 15
+			},
+			boundaryGap: false,
+			data: ddate_array,
 
+			axisLabel: {
+				color: "white",
+			}
 
-function draw_active_areachart(idname,ddate_array ,active_array) {
-    var mychart = echarts.init(document.getElementById(idname));
-    option = {
-           title:{
-               textStyle:{
-                   color:'white'
-               }
-           },
-        tooltip: {
-            trigger: 'axis',
-            axisPointer: {
-                type: 'cross',
-                label: {
-                    backgroundColor: '#6a7985'
-                }
-            }
-        },
-        xAxis: {
-            type: 'category',
-            name: "时间",
-            nameLocation: "middle",
-            nameGap: 30,
-            nameTextStyle: {
-                color: "white",
-                fontSize: 15
-            },
-            boundaryGap: false,
-            data:ddate_array,
+		},
+		yAxis: {
+			type: 'value',
 
-            axisLabel: {
-                color: "white",
-            }
+			name: "活跃数",
+			nameLocation: "middle",
+			nameGap: 50,
+			nameTextStyle: {
+				color: "white",
+				fontSize: 15,
 
-        },
-        yAxis: {
-            type: 'value',
+			},
 
-            name: "活跃数",
-            nameLocation: "middle",
-            nameGap: 50,
-            nameTextStyle: {
-                color: "white",
-                fontSize: 15,
+			axisLabel: {
+				color: "white",
+			}
 
-            },
+		},
 
-            axisLabel: {
-                color: "white",
-            }
+		series: [{
+			name: "活跃数",
+			data: active_array,
+			type: 'line',
+			areaStyle: {},
+		}]
+	};
 
-        },
-
-        series: [
-            {
-                name: "活跃数",
-                data: active_array,
-                type: 'line',
-                areaStyle: {},
-            }]
-    };
-   
-    mychart.setOption(option);
-
-
-
+	mychart.setOption(option);
 
 }
