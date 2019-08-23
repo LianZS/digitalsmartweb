ab940b70537ce947350630279bca2a039f6ab055
LianZS <2214057556@qq.com>
2019-08-23 12:14:09 +0800

    音乐链接请求

--- interterface/js/search.js
+++ interterface/js/search.js
@@ -1,12 +1,24 @@
 function search() {
-	let appname = $("#appname").val()
-	if(appname==""){
+	let musicname = $("#music-name").val()
+	if(musicname==""){
 		return 
 	}
+	let url =" http://127.0.0.1:8000/interface/api/getMusic?"
+	soft_type = $("input[type='radio']:checked").val();
+	$.get(url,{
+		"name":musicname,
+		"type":soft_type
+	},function(data){
+		data =data['data']
+		for(let i=0;i<data.length;i++){
+			author = data[i]['author']
+			title =data[i]['title']
+			url =data[i]['url']
+			li ="<li class=\"aplayer-list-light\"><span class=\"aplayer-list-title\">"+title+"</span><span class=\"aplayer-list-author\" style=\"float: right;\">"+author+"</span><span class=\"aplayer-url\" value="+url+"></span></li>"
+			$("ol").append(li)
+		}
+	},'json')
 	
-	href =$("#search-href").attr('href')+"?keyword="+appname
-	$("#search-href").attr("href",href)
-	$("#search-href")[0].click()
 
 
 }
