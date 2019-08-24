8cb9e47a2e4310b38ead9a7afa40711b44b2bf94
LianZS <2214057556@qq.com>
2019-08-24 12:11:42 +0800

    pdf转换页面

--- interterface/js/search.js
+++ interterface/js/search.js
@@ -3,6 +3,8 @@ var soft_type;
 var page = 1;
 
 function search() {
+	let url = " http://127.0.0.1:8000/interface/api/getMusic?"
+
 	$("#aplayer-list").css("display", "none")
 	$(".m-load2").css("display", "")
 	$(".music-audio").empty()
@@ -12,7 +14,6 @@ function search() {
 		return
 	}
 	page = 1
-	let url = " http://127.0.0.1:8000/interface/api/getMusic?"
 	soft_type = $("input[type='radio']:checked").val();
 	$.get(url, {
 		"name": musicname,
@@ -21,9 +22,9 @@ function search() {
 	}, function(data) {
 		$("ol").empty()
 		data = data['data']
-		if(data.length==0){
+		if(data.length == 0) {
 			alert("不好意思，该找不到相关歌曲！")
-				$(".m-load2").css("display", "none")
+			$(".m-load2").css("display", "none")
 
 			return
 		}
@@ -69,4 +70,67 @@ function search() {
 
 }
 
+function choice_file() {
+	$("#pdffile").change(function() {
 
+		filepath = $("#pdffile").val()
+		$("#text").html(filepath);
+	})
+	$("#btn").click(function() {
+		if(filepath == "") {
+			alert("未选择文件")
+			return
+		} else {
+			$(".m-load2").css("display", "")
+			var formData = new FormData();
+			f = $("#pdffile")[0].files[0]
+			formData.append("pdf", f);
+
+			$.ajax({
+					url: 'http://127.0.0.1:8000/interface/api/uploadPDF',
+					type: 'POST',
+					cache: true,
+					data: formData,
+					processData: false,
+					contentType: false
+				}).done(function(data) {
+					uid = data['id']
+					let url = "http://127.0.0.1:8000/interface/api/getDocLink?"
+					var code = 0
+					var p = 0
+					for(let i = 0; i < 10; i++) {
+						if(code == 1 & p == 100) {
+							break
+						}
+						sleep(1000)
+						$.ajaxSettings.async = false;
+						$.get(url, {
+							"id": uid
+						}, function(data) {
+							code = data['code']
+							p = data['p']
+							if(code == 1 & p == 100) {
+								$(".m-load2").css("display", "none")
+
+								$(".down").css("display", "")
+//								alert("转换成功")
+
+							}
+						}, 'json')
+					}
+				})
+				.fail(function(res) {});
+
+		}
+	})
+
+}
+
+function sleep(n) {
+	var start = new Date().getTime();
+	while(true) {
+		if(new Date().getTime() - start > n) {
+			break;
+		}
+	}
+}
